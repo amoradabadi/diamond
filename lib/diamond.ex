b0bc313eb77a5d65248c3b259bc5f071b12f4621
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-29 16:07:13 +0200

    leading spaces for b and c

--- lib/diamond.ex
+++ lib/diamond.ex
@@ -2,22 +2,22 @@ defmodule Diamond do
   def printDiamond(char) do
     if char == 'D' do
       """
-         A
-        #{middle_bit("B")}
-       #{middle_bit("C")}
-      #{middle_bit("D")}
-       #{middle_bit("C")}
-        #{middle_bit("B")}
-         A
+      #{leading_spaces_D("A")}A
+      #{leading_spaces_D("B")}#{middle_bit("B")}
+      #{leading_spaces_D("C")}#{middle_bit("C")}
+      #{leading_spaces_D("D")}#{middle_bit("D")}
+      #{leading_spaces_D("C")}#{middle_bit("C")}
+      #{leading_spaces_D("B")}#{middle_bit("B")}
+      #{leading_spaces_D("A")}A
       """
     else
       if char == 'C' do
         """
-          A
-         #{middle_bit("B")}
-        #{middle_bit("C")}
-         #{middle_bit("B")}
-          A
+        #{leading_spaces_C("A")}A
+        #{leading_spaces_C("B")}#{middle_bit("B")}
+        #{leading_spaces_C("C")}#{middle_bit("C")}
+        #{leading_spaces_C("B")}#{middle_bit("B")}
+        #{leading_spaces_C("A")}A
         """
       else
         if char == 'B' do
@@ -42,6 +42,27 @@ defmodule Diamond do
       "B" => 1,
       "C" => 3,
       "D" => 5
+    }
+
+    String.duplicate(" ", map[char])
+  end
+
+  defp leading_spaces_D(char) do
+    map = %{
+      "A" => 3,
+      "B" => 2,
+      "C" => 1,
+      "D" => 0
+    }
+
+    String.duplicate(" ", map[char])
+  end
+
+  defp leading_spaces_C(char) do
+    map = %{
+      "A" => 2,
+      "B" => 1,
+      "C" => 0
     }
 
     String.duplicate(" ", map[char])
