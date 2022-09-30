b0423810dfe8dab058cd881bb6f30d31d91443c6
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-30 09:45:03 +0200

    use libe for char

--- lib/diamond.ex
+++ lib/diamond.ex
@@ -3,27 +3,27 @@ defmodule Diamond do
     if diamond_type == "D" do
       """
       #{leading_spaces(diamond_type, "A")}A
-      #{leading_spaces(diamond_type, "B")}#{middle_bit("B")}
-      #{leading_spaces(diamond_type, "C")}#{middle_bit("C")}
-      #{leading_spaces(diamond_type, "D")}#{middle_bit("D")}
-      #{leading_spaces(diamond_type, "C")}#{middle_bit("C")}
-      #{leading_spaces(diamond_type, "B")}#{middle_bit("B")}
+      #{line_for_char(diamond_type, "B")}
+      #{line_for_char(diamond_type, "C")}
+      #{line_for_char(diamond_type, "D")}
+      #{line_for_char(diamond_type, "C")}
+      #{line_for_char(diamond_type, "B")}
       #{leading_spaces(diamond_type, "A")}A
       """
     else
       if diamond_type == "C" do
         """
         #{leading_spaces(diamond_type, "A")}A
-        #{leading_spaces(diamond_type, "B")}#{middle_bit("B")}
-        #{leading_spaces(diamond_type, "C")}#{middle_bit("C")}
-        #{leading_spaces(diamond_type, "B")}#{middle_bit("B")}
+        #{line_for_char(diamond_type, "B")}
+        #{line_for_char(diamond_type, "C")}
+        #{line_for_char(diamond_type, "B")}
         #{leading_spaces(diamond_type, "A")}A
         """
       else
         if diamond_type == "B" do
           """
           #{leading_spaces(diamond_type, "A")}A
-          #{leading_spaces(diamond_type, "B")}#{middle_bit("B")}
+          #{line_for_char(diamond_type, "B")}
           #{leading_spaces(diamond_type, "A")}A
           """
         else
@@ -31,6 +31,10 @@ defmodule Diamond do
         end
       end
     end
+  end
+
+  defp line_for_char(diamond_type, char) do
+    "#{leading_spaces(diamond_type, char)}#{middle_bit(char)}"
   end
 
   defp middle_bit(char) do
